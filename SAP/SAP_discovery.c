98900b08197cbc572d3639bb798893a7eb32e8a9
Robin S <rsham@free.fr>
2019-08-24 18:15:44 +0200

    Improved readability

--- SAP/SAP_discovery.c
+++ SAP/SAP_discovery.c
@@ -1,12 +1,8 @@
 /*
 
-multicast.c
+SAP_discovery.c
 
-The following program sends or receives multicast packets. If invoked
-with one argument, it sends a packet containing the current time to an
-arbitrarily chosen multicast group and UDP port. If invoked with no
-arguments, it receives and prints these packets. Start it as a sender on
-just one host and as a receiver on all the other hosts
+Discover SAP announcement of Dante streams.
 
 */
 
@@ -20,72 +16,66 @@ just one host and as a receiver on all the other hosts
 #include <unistd.h>
 #include <string.h>
 
-#define EXAMPLE_PORT 9875
-#define EXAMPLE_GROUP "239.255.255.255"
+#define SAP_MULTICAST_PORT 9875
+#define SAP_MULTICAST_IP "239.255.255.255"
 
 int main(int argc, char *argv[])
 {
-   struct sockaddr_in addr;
-   int addrlen, sock, cnt;
-   struct ip_mreq mreq;
-   char message[1024];
+	struct sockaddr_in addr;
+	int addrlen, sap_socket, cnt;
+	struct ip_mreq mreq;
+	char message[1024];
 
-   /* set up socket */
-   sock = socket(AF_INET, SOCK_DGRAM, 0);
-   if (sock < 0) {
-     perror("socket");
-     exit(1);
-   }
-   bzero((char *)&addr, sizeof(addr));
-   addr.sin_family = AF_INET;
-   addr.sin_addr.s_addr = htonl(INADDR_ANY);
-   addr.sin_port = htons(EXAMPLE_PORT);
-   addrlen = sizeof(addr);
+	/* set up socket */
+	sap_socket = socket(AF_INET, SOCK_DGRAM, 0);
+	if(sap_socket < 0)
+	{
+		perror("Error opening SAP socket");
+		exit(1);
+	}
 
-   if (argc > 1) {
-      /* send */
-      addr.sin_addr.s_addr = inet_addr(EXAMPLE_GROUP);
-      while (1) {
-	 time_t t = time(0);
-	 sprintf(message, "time is %-24.24s", ctime(&t));
-	 printf("sending: %s\n", message);
-	 cnt = sendto(sock, message, sizeof(message), 0,
-		      (struct sockaddr *) &addr, addrlen);
-	 if (cnt < 0) {
- 	    perror("sendto");
-	    exit(1);
-	 }
-	 sleep(5);
-      }
-   } else {
+	bzero((char *)&addr, sizeof(addr));
+	addr.sin_family = AF_INET;
+	addr.sin_addr.s_addr = htonl(INADDR_ANY);
+	addr.sin_port = htons(SAP_MULTICAST_PORT);
+	addrlen = sizeof(addr);
 
-      /* receive */
-      if (bind(sock, (struct sockaddr *) &addr, sizeof(addr)) < 0) {
-         perror("bind");
-	 		 	 exit(1);
-      }
-      mreq.imr_multiaddr.s_addr = inet_addr(EXAMPLE_GROUP);
-      mreq.imr_interface.s_addr = htonl(INADDR_ANY);
-      if (setsockopt(sock, IPPROTO_IP, IP_ADD_MEMBERSHIP,
-		     &mreq, sizeof(mreq)) < 0) {
-	 perror("setsockopt mreq");
-	 exit(1);
-      }
-      while (1) {
- 	 cnt = recvfrom(sock, message, sizeof(message), 0,
-			(struct sockaddr *) &addr, &addrlen);
-	 if (cnt < 0) {
-	    perror("recvfrom");
-	    exit(1);
-	 } else if (cnt == 0) {
- 	    break;
-	 }
-	 printf("=== %s: %d byte SAP packet :\n=== TYPE : %s\n\n%s\n\n\n\n", inet_ntoa(addr.sin_addr), cnt-24, &message[8], &message[24]);
-	 // for(int i = 0 ; i < cnt ; i++)
-	 // {
-		//  printf("message[%d] : %X\t%c\n", i, message[i], message[i]);
-	 // }
+	/* receive */
+	if(bind(sap_socket, (struct sockaddr *) &addr, sizeof(addr)) < 0)
+	{
+		perror("Error binding SAP socket");
+		exit(1);
+	}
 
-        }
-    }
+	mreq.imr_multiaddr.s_addr = inet_addr(SAP_MULTICAST_IP);
+	mreq.imr_interface.s_addr = htonl(INADDR_ANY);
+
+	if(setsockopt(sap_socket, IPPROTO_IP, IP_ADD_MEMBERSHIP, &mreq, sizeof(mreq)) < 0)
+	{
+		perror("Error subscribing to SAP multicast group");
+		exit(1);
+	}
+
+	while (1)
+	{
+		memset(message, 0, sizeof(message));
+
+		cnt = recvfrom(sap_socket, message, sizeof(message), 0,	(struct sockaddr *) &addr, &addrlen);
+		if(cnt < 0)
+		{
+			perror("Error receiving SAP data");
+			exit(1);
+		}
+		else if(cnt == 0)
+			break;
+
+			printf("=== %s: %d byte SAP packet with %d byte SDP description :\n", inet_ntoa(addr.sin_addr), cnt, cnt-24);
+			printf("=== TYPE : %s\n\n", &message[8]);
+			printf("%s\n\n\n\n", &message[24]);
+
+			// for(int i = 0 ; i < cnt ; i++)
+			// {
+			// 	printf("message[%d] : %X\t%c\n", i, message[i], message[i]);
+			// }
+		}
 }
