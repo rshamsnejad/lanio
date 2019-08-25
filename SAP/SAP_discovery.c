ff854b8953b60d4d7f2b4a05e72117b0f2565054
Robin S <rsham@free.fr>
2019-08-25 21:23:02 +0200

    Rewrote SAP discovery to use GLib

--- SAP/SAP_discovery.c
+++ SAP/SAP_discovery.c
@@ -6,83 +6,110 @@ Discover SAP announcement of Dante streams.
 
 */
 
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <arpa/inet.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <string.h>
+#include "../headers/linux-aes67.h"
 
-#ifndef TRUE
-	#define TRUE 1
-#endif
-
-#ifndef FALSE
-	#define FALSE 0
-#endif
-
+#define SAP_MULTICAST_ADDRESS "239.255.255.255"
 #define SAP_MULTICAST_PORT 9875
-#define SAP_MULTICAST_IP "239.255.255.255"
 
-int main(int argc, char *argv[])
+// SAP Socket local address is set to 0.0.0.0/<port>
+// in order to listen to all local interfaces
+#define SAP_LOCAL_ADDRESS "0.0.0.0"
+
+#define SAP_PACKET_BUFFER_SIZE 1024
+
+#define SAP_PACKET_PREHEADER_SIZE 8
+#define SAP_PACKET_HEADER_SIZE 24
+
+
+gint main(gint argc, gchar *argv[])
 {
-	struct sockaddr_in addr;
-	int addrlen, sap_socket, cnt;
-	struct ip_mreq mreq;
-	char message[1024];
 
-	// Set up UDP/IPv4 socket
-	sap_socket = socket(AF_INET, SOCK_DGRAM, 0);
-	if(sap_socket < 0)
-	{
-		perror("Error opening SAP socket");
-		exit(1);
-	}
+	// Set up an UDP/IPv4 socket for SAP discovery
 
-	memset((char *)&addr, 0, sizeof(addr));
-	addr.sin_family = AF_INET;
-	addr.sin_addr.s_addr = htonl(INADDR_ANY);
-	addr.sin_port = htons(SAP_MULTICAST_PORT);
-	addrlen = sizeof(addr);
+	GSocket *SAPSocket = NULL;
+	GError *SAPSocketOpenError = NULL;
 
-	/* receive */
-	if(bind(sap_socket, (struct sockaddr *) &addr, sizeof(addr)) < 0)
-	{
-		perror("Error binding SAP socket");
-		exit(1);
-	}
+	SAPSocket = g_socket_new
+							(
+								G_SOCKET_FAMILY_IPV4,
+								G_SOCKET_TYPE_DATAGRAM,
+								G_SOCKET_PROTOCOL_DEFAULT,
+								&SAPSocketOpenError
+							);
 
-	mreq.imr_multiaddr.s_addr = inet_addr(SAP_MULTICAST_IP);
-	mreq.imr_interface.s_addr = htonl(INADDR_ANY);
+	ProcessGError("Error opening SAP socket", SAPSocketOpenError);
 
-	if(setsockopt(sap_socket, IPPROTO_IP, IP_ADD_MEMBERSHIP, &mreq, sizeof(mreq)) < 0)
-	{
-		perror("Error subscribing to SAP multicast group");
-		exit(1);
-	}
+
+	// Bind the SAP socket to listen to all local interfaces
+
+	GInetAddress *SAPLocalAddress = g_inet_address_new_from_string
+																	(SAP_LOCAL_ADDRESS);
+	GSocketAddress *SAPSocketAddress = g_inet_socket_address_new
+																		 (SAPLocalAddress, SAP_MULTICAST_PORT);
+	GError *SAPSocketBindError = NULL;
+
+	g_socket_bind(SAPSocket, SAPSocketAddress, TRUE, &SAPSocketBindError);
+
+	ProcessGError("Error binding SAP socket", SAPSocketBindError);
+
+
+	// Subscribe to the SAP Multicast group
+
+	GInetAddress *SAPMulticastAddress = g_inet_address_new_from_string
+																			(SAP_MULTICAST_ADDRESS);
+	GError *SAPMulticastJoinError = NULL;
+
+	g_socket_join_multicast_group
+	(
+		SAPSocket,
+		SAPMulticastAddress,
+		FALSE,
+		NULL,
+		&SAPMulticastJoinError
+	);
+
+	ProcessGError("Error joining SAP Multicast group", SAPMulticastJoinError);
+
+
+	// Begin the SAP packet receiving loop
+
+	gchar SAPPacketString[SAP_PACKET_BUFFER_SIZE];
+	gssize SAPPacketStringBytesRead = 0;
+	GSocketAddress *SAPPacketSourceSocket = NULL;
+	GInetAddress *SAPPacketSourceAddress = NULL;
+	GError *SAPPacketError = NULL;
 
 	while(TRUE)
 	{
-		memset(message, 0, sizeof(message));
+		// Reinitialize the packet string buffer
+		memset(SAPPacketString, '\0', sizeof(SAPPacketString));
 
-		cnt = recvfrom(sap_socket, message, sizeof(message), 0,	(struct sockaddr *) &addr, &addrlen);
-		if(cnt < 0)
-		{
-			perror("Error receiving SAP data");
-			exit(1);
-		}
-		else if(cnt == 0)
+		SAPPacketStringBytesRead = g_socket_receive_from
+															 (
+																 SAPSocket,
+																 &SAPPacketSourceSocket,
+																 SAPPacketString,
+																 sizeof(SAPPacketString),
+																 NULL,
+																 &SAPPacketError
+															 );
+		if(SAPPacketStringBytesRead < 0)
+			ProcessGError("Error receiving SAP packet", SAPPacketError);
+		else if(SAPPacketStringBytesRead == 0) // The connection has been closed
 			break;
 
-		printf("=== %s: %d byte SAP packet with %d byte SDP description :\n", inet_ntoa(addr.sin_addr), cnt, cnt-24);
-		printf("=== TYPE : %s\n\n", &message[8]);
-		printf("%s\n\n\n\n", &message[24]);
+		SAPPacketSourceAddress = g_inet_socket_address_get_address
+														 ((GInetSocketAddress *) SAPPacketSourceSocket);
 
-		// for(int i = 0 ; i < cnt ; i++)
-		// {
-		// 	printf("message[%d] : %X\t%c\n", i, message[i], message[i]);
-		// }
+		printf
+		(
+			"=== %s: %d byte SAP packet with %d byte SDP description :\n",
+			g_inet_address_to_string(SAPPacketSourceAddress),
+			SAPPacketStringBytesRead,
+			SAPPacketStringBytesRead-SAP_PACKET_HEADER_SIZE
+		);
+		printf("=== TYPE : %s\n\n", &SAPPacketString[SAP_PACKET_PREHEADER_SIZE]);
+		printf("%s\n\n\n\n", &SAPPacketString[SAP_PACKET_HEADER_SIZE]);
+
 	}
 }
