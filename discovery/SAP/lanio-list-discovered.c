28086eb846d3bc62c91f8090492c86fad4e85b51
Robin Shamsnejad <rsham@free.fr>
2019-09-10 17:52:32 +0200

    Added second brick : lanio-list-discovered

---
+++ discovery/SAP/lanio-list-discovered.c
@@ -0,0 +1,62 @@
+/*
+
+lanio-list-discovered.c
+
+List available streams discovered by lanio-discovery.
+
+*/
+
+////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+
+#include "../../headers/lanio.h"
+
+////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+
+gint main(gint argc, gchar *argv[])
+{
+    g_debug
+    (
+        "GLib version %u.%u.%u",
+        glib_major_version,
+        glib_minor_version,
+        glib_micro_version
+    );
+
+    // Parse the command-line options
+    /*
+    DiscoveryCLIParameters CommandLineParameters;
+    initDiscoveryCLIParameters(&CommandLineParameters);
+    parseDiscoveryCommandLineOptions
+    (
+        &CommandLineParameters,
+        argc,
+        argv
+    );
+    */
+
+    // Set up the SDP Database file
+    gchar *SDPDatabasePath = getSDPDatabasePath();
+    sqlite3 *SDPDatabase = NULL;
+    processSQLiteOpenError
+    (
+        sqlite3_open_v2
+        (
+            SDPDatabasePath,
+            &SDPDatabase,
+            SQLITE_OPEN_READONLY,
+            NULL
+        )
+    );
+    g_free(SDPDatabasePath);
+
+    g_print("List of Streamz\n");
+
+    sqlite3_close(SDPDatabase);
+
+    g_debug("Reached end of main() in lanio-list-discovered.c");
+    return EXIT_SUCCESS;
+}
