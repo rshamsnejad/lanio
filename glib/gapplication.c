3e1de7c833169a7ed09d9e81dcf570d484dcec80
Robin Shamsnejad <rsham@free.fr>
2019-10-30 14:34:29 +0100

    Functional GApplication

--- glib/gapplication.c
+++ glib/gapplication.c
@@ -1,39 +1,41 @@
 #include <glib.h>
 #include <gio/gio.h>
+#include <glib-unix.h>
 #include <stdlib.h>
 
-void countdown(void)
-{
-    for(gsize i = 5 ; i >= 1 ; i--)
-    {
-        g_print("%" G_GSIZE_FORMAT "\n", i);
-        g_usleep(1 * G_USEC_PER_SEC);
-    }
-}
-
-void dieIfRemote(GApplication *App)
-{
-    if(g_application_get_is_remote(App))
-    {
-        g_printerr("Already running.\n");
-        exit(EXIT_FAILURE);
-    }
-}
 
 void activate(GApplication *App)
 {
-    dieIfRemote(App);
-
     g_print("I'm activated !\n");
-    countdown();
 }
 
 void startup(GApplication *App)
 {
-    dieIfRemote(App);
-
+    g_application_hold(App);
     g_print("I'm started !\n");
-    countdown();
+}
+
+gboolean printShit(gpointer Data)
+{
+    gchar *String = (gchar*) Data;
+    g_print("%s", String);
+    return TRUE;
+}
+
+gboolean callback_terminate(gpointer Data)
+{
+    gint *Signal = (gint*) Data;
+    gchar *String = NULL;
+
+    if(*Signal == SIGINT)
+        String = "Keyboard Interrupt";
+    else if(*Signal == SIGTERM)
+        String = "SIGTERM";
+
+    g_printerr("%s. Terminating\n", String);
+
+    // g_free(String);
+    exit(EXIT_FAILURE);
 }
 
 int main(int argc, char *argv[])
@@ -41,13 +43,32 @@ int main(int argc, char *argv[])
     GApplication *App;
     gint Status;
 
-    App = g_application_new("io.myapp.test", 0);
+    App = g_application_new("io.myapp.test", G_APPLICATION_IS_SERVICE);
+
     g_signal_connect(App, "activate", G_CALLBACK(activate), NULL);
     g_signal_connect(App, "startup", G_CALLBACK(startup), NULL);
 
-    g_application_register(App, NULL, NULL);
+    g_timeout_add
+    (
+        1 * 1000,
+        (GSourceFunc) printShit,
+        "LOL\n"
+    );
 
-    dieIfRemote(App);
+    gint SignalInterrupt = SIGINT;
+    g_unix_signal_add
+    (
+        SignalInterrupt,
+        (GSourceFunc) callback_terminate,
+        &SignalInterrupt
+    );
+    gint SignalTerminate = SIGTERM;
+    g_unix_signal_add
+    (
+        SignalTerminate,
+        (GSourceFunc) callback_terminate,
+        &SignalTerminate
+    );
 
     Status = g_application_run(G_APPLICATION(App), argc, argv);
     g_object_unref(App);
