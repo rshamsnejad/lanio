9b2c3914759e0104d2b1a7b1f7df75178c8e43aa
Robin S <rsham@free.fr>
2019-11-10 21:12:31 +0100

    Added ALSA test sink

--- audioio/sdp_pipeline.sh
+++ audioio/sdp_pipeline.sh
@@ -1,1 +1,3 @@
-gst-launch-1.0 sdpsrc location="sdp://$1" ! decodebin ! autoaudiosink
+#gst-launch-1.0 sdpsrc location="sdp://$1" ! decodebin ! autoaudiosink
+
+gst-launch-1.0 sdpsrc location="sdp://$1" ! decodebin ! audioconvert ! audioresample ! alsasink device=hw:CARD=I82801AAICH,DEV=0
