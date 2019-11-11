91649a022c727cc9e3e012073e29333ced81a7df
Robin S <rsham@free.fr>
2019-11-11 14:40:07 +0100

    Aired up the code a bit

--- audioio/lanio-receive.c
+++ audioio/lanio-receive.c
@@ -6,40 +6,39 @@ Receive AES67 streams discovered by lanio-discovery.
 
 */
 
-////////////////////////////////////////////////////////////////////////////////
-////////////////////////////////////////////////////////////////////////////////
-////////////////////////////////////////////////////////////////////////////////
-
 #include "../headers/lanio.h"
 
-////////////////////////////////////////////////////////////////////////////////
-////////////////////////////////////////////////////////////////////////////////
-////////////////////////////////////////////////////////////////////////////////
 
 gint main(gint argc, gchar *argv[])
 {
-// Parse the command-line options
+
+////////////////////////////////// INIT ////////////////////////////////////////
+
+        // Parse the command-line options
     ReceiveCLIParameters CommandLineParameters;
     initReceiveCLIParameters(&CommandLineParameters);
 
     parseReceiveCLIOptions(&CommandLineParameters, argc, argv);
 
-// Redirect the logs to stdout/stderr or journald
-// depending on the CLI parameters, and handle log levels
+        // Redirect the logs to stdout/stderr or journald
+        // depending on the CLI parameters, and handle log levels
     setLogHandler(CommandLineParameters.Terminal, CommandLineParameters.Debug);
 
-// Print library versions to debug canal
+        // Print library versions to debug canal
     printLibraryVersions();
 
-// Create and store the working directories' paths
+        // Create and store the working directories' paths
     WorkingDirectoryList WorkingDirectories;
     initWorkingDirectoryList(&WorkingDirectories);
 
-// Set up the SDP Database file
+        // Set up the SDP Database file
     sqlite3 *SDPDatabase = NULL;
     openSDPDatabase(&SDPDatabase, SQLITE_OPEN_READONLY, &WorkingDirectories);
 
-// -- BEGIN ACTUAL PROGRAM --
+////////////////////////////////////////////////////////////////////////////////
+
+
+////////////////////////////////// CORE ////////////////////////////////////////
 
     g_info("Receiving Stream ID %u", CommandLineParameters.StreamID);
     gchar *ReturnedSDP =
@@ -52,9 +51,16 @@ gint main(gint argc, gchar *argv[])
         g_info
             ("Outputting to ALSA Device %s", CommandLineParameters.ALSADevice);
 
-// Clean up
+////////////////////////////////////////////////////////////////////////////////
+
+
+/////////////////////////////// CLEANUP ////////////////////////////////////////
+
     sqlite3_close(SDPDatabase);
     freeWorkingDirectoryList(&WorkingDirectories);
+
+////////////////////////////////////////////////////////////////////////////////
+
 
     g_debug("Reached end of main() in lanio-receive.c");
     return EXIT_SUCCESS;
